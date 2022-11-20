6047f0b8691d8fd13acde73c3530cadfc46521f5
rahmatfadj <adjie1476@gmail.com>
2022-11-20 23:46:32 +0800

    MainMenu Scene & Game UI

---
+++ Assets/Scripts/Rahmat/CanvasGame.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class CanvasGame : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public Button pauseButton;
+    public Button resumeButton;
+    public Button backToMenuButton;
+    public TextMeshProUGUI healthUI;
+    // public PlayerController player;
+    private void Awake()
+    {
+        pauseButton.onClick.AddListener(() => PauseButton());
+        backToMenuButton.onClick.AddListener(() => BackToMenuButton());
+        resumeButton.onClick.AddListener(() => Resume());
+        // player = FindObjectOfType<PlayerController>();
+    }
+    private void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+    private void Update()
+    {
+        // healthUI.text = player.health;
+    }
+
+    private void PauseButton()
+    {
+        if (!pauseUI.activeSelf)
+        {
+            Time.timeScale = 0;
+            pauseUI.SetActive(true);
+        }
+        else if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 1;
+            pauseUI.SetActive(false);
+        }
+    }
+    private void Resume()
+    {
+        Time.timeScale = 1;
+        pauseUI.SetActive(false);
+    }
+    private void BackToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
